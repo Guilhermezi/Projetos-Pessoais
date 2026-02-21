7096bb43b52f292cc0ae98a37d8ecc63608a1595
Guilherme Izidio Nogueira <guilherme.elinogueira@gmail.com>
2026-02-21 17:00:14 -0300

    JAVA and C
    Just because I need to learn this

---
+++ C/Variáveis/Nomenclatura.c
@@ -0,0 +1,10 @@
+/*
+Desafio
+
+Fácil
+Escreva um programa que declara e inicializa duas variáveis com nomes válidos de acordo com as regras de nomenclatura do C. 
+A primeira variável deve ser um inteiro chamado "userAge" definido como 25. A segunda variável deve ser um float chamado "item_price" definido como 9.99. 
+Em seguida, imprima ambas as variáveis usando printf no seguinte formato:
+User age: 25
+Item price: 9.99
+*/
---
+++ Java/desafioDiario/desafio_01.java
@@ -0,0 +1,18 @@
+/*
+Desafio
+
+Iniciante
+Inicialize as seguintes variáveis:
+
+us: String com o valor "United States"
+population: int com o valor 331002651
+*/
+class Main {
+    public static void main(String[] args) {
+        String us = "United States";
+        int population = 331002651;
+    
+        System.out.println("us = " + us);
+        System.out.println("population = " + population);
+    }
+}

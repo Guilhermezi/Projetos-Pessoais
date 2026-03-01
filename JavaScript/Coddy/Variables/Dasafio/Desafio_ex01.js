864313d77802501edbbc127c2e74b0b41da61d76
Guilherme Izidio Nogueira <guilherme.elinogueira@gmail.com>
2026-03-01 13:35:37 -0300

    JS
    Just easy challenges

---
+++ JavaScript/Coddy/Variables/Dasafio/Desafio_ex01.js
@@ -0,0 +1,12 @@
+/*
+Desafio
+
+Iniciante
+Declare uma variável chamada isLoggedIn e atribua a ela o valor false para representar que um usuário não está atualmente logado.
+*/
+
+// Type your code below
+let isLoggedIn = false
+
+// Don't change the line below
+console.log(`User logged in: ${isLoggedIn}`)
---
+++ JavaScript/Coddy/Variables/Dasafio/Desafio_ex02.js
@@ -0,0 +1,14 @@
+/* 
+Desafio
+
+Iniciante
+O código a seguir contém declarações de variáveis, mas nem todas seguem as convenções de nomenclatura adequadas do JavaScript. Sua tarefa é identificar e remover as variáveis que não seguem as convenções de nomenclatura. As convenções de nomenclatura adequadas incluem:
+
+Os nomes das variáveis devem ser escritos em camelCase.
+Evite usar palavras-chave reservadas ou começar com um número.
+Os nomes das variáveis devem ser descritivos e evitar caracteres desnecessários.
+Sem espaços ou caracteres especiais (exceto _ e $ se necessário).
+Alguns dos nomes podem gerar erros, você deve excluí-los!
+
+Você pode retornar ao código padrão a qualquer momento clicando no botão no canto superior direito no editor de código.
+*/

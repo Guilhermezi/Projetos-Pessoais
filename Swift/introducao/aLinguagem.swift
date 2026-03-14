365e419bad99c2948dbbb887d9e6550873d29013
Guilherme Izidio Nogueira <guilherme.elinogueira@gmail.com>
2026-03-14 20:23:17 -0300

    swift
    slow and steady

---
+++ Swift/introducao/aLinguagem.swift
@@ -0,0 +1,16 @@
+/*
+Swift é uma linguagem de programação poderosa e intuitiva desenvolvida pela Apple para criar aplicativos em iOS, macOS, watchOS e tvOS.
+
+
+Desafio
+
+Iniciante
+Bem-vindo ao seu primeiro programa Swift! O código já está escrito para você.
+
+O que fazer:
+
+Olhe para o código: print("Hello Swift!")
+Pressione o botão "Run Code" para executá-lo
+Você deve ver "Hello Swift!" aparecer na saída
+*/
+print("Hello Swift!")
---
+++ Swift/introducao/helloWorld.swift
@@ -0,0 +1,21 @@
+/*
+O "Hello World!" é um programa simples que exibe Hello World! na tela.
+
+Em Swift, usamos print() para mostrar palavras na tela. As palavras vão dentro de aspas.
+
+Vamos dar uma olhada no programa "Hello World!" em Swift:
+
+print("Hello World!")
+
+Desafio
+
+Iniciante
+Use o editor de código para escrever um programa que imprima Hello World!
+
+Observe que qualquer coisa dentro das aspas é sensível a maiúsculas e minúsculas. Por exemplo:
+
+print("Hello World!")
+print("hello world!")
+são coisas diferentes (observe as letras maiúsculas na primeira linha).
+*/
+print("Hello World!")

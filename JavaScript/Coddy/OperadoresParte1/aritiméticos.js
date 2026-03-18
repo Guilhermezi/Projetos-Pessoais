286ca5bf7d09f71af088d9b27c0a596f1316c0bb
Guilherme Izidio Nogueira <guilherme.elinogueira@gmail.com>
2026-03-17 21:10:43 -0300

    Js
    That's all; I don't really feel like coding

---
+++ JavaScript/Coddy/OperadoresParte1/aritiméticos.js
@@ -0,0 +1,23 @@
+/*
+Operadores são usados para realizar operações em valores.
+
+Primeiro, discutiremos os operadores aritméticos mais básicos, eles podem ser familiares das aulas de matemática.
+
+Operador	Operação	Exemplo
++	Adição	3 + 2 = 5
+-	Subtração	3 - 2 = 1
+*	Multiplicação	3 * 2 = 6
+/	Divisão	4 / 2 = 2
+Vamos ver um exemplo de uso,
+
+let a = 3
+let b = 5
+let c = a + b // c contém 8
+
+Desafio
+
+Iniciante
+Escreva um código que inicializa duas variáveis, a e b, com os valores 5.2 e 2.6 (respectivamente).
+
+Depois disso, inicialize outra variável c que conterá o resultado de a / b.
+*/

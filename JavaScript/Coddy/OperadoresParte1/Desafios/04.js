2bfe71a79aadb6da79579bda70f4d585fc006c92
Guilherme Izidio Nogueira <guilherme.elinogueira@gmail.com>
2026-05-10 21:59:14 -0300

    JS
    Just to keep up the tradition, I’m going to make JavaScript the programming language I master best in a single day, and today is day one.

---
+++ JavaScript/Coddy/OperadoresParte1/Desafios/04.js
@@ -0,0 +1,11 @@
+/*
+Escreva um script que inicializa uma variável age com o valor 18.
+
+Depois disso, inicialize outra variável canVote que conterá se a pessoa tem idade suficiente para votar (18 ou mais).
+*/
+// Type your code below
+var age = 18
+var canVote = age >= 18
+
+// Don't change the line below
+console.log(`age = ${age}, canVote = ${canVote}`)
